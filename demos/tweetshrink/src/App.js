3e10b0e3c55dbe15c21d4158aca08c723797b402
Clinton Dreisbach <clinton@dreisbach.us>
2020-04-15 09:57:10 -0400

    Convert tweetshrink to use hooks

--- demos/tweetshrink/src/App.js
+++ demos/tweetshrink/src/App.js
@@ -1,93 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'tachyons/css/tachyons.min.css'
 import textOptions from './textOptions'
 import TextDisplay from './components/TextDisplay'
 import TextOptionField from './components/TextOptionField'
 
-class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      text: '',
-      options: []
-    }
+function App () {
+  const [text, setText] = useState('')
+  const [options, setOptions] = useState([])
 
-    this.handleTextUpdate = this.handleTextUpdate.bind(this)
-  }
-
-  handleTextUpdate (event) {
-    this.setState({ text: event.target.value })
-  }
-
-  setOption (option) {
+  function setOption (option) {
     return (event) => {
       const checked = event.target.checked
-      const optionSet = new Set(this.state.options)
+
+      let newOptions = [...options]
       if (checked) {
-        optionSet.add(option)
+        newOptions.push(option)
       } else {
-        optionSet.delete(option)
+        newOptions = newOptions.filter(o => o !== option)
       }
-      this.setState({
-        options: [...optionSet]
-      })
 
-      // Alternative method
-
-      // const options = this.state.options
-
-      // if (checked && !options.includes(option)) {
-      //   options.push(option)
-      // }
-      // if (!checked && options.includes(option)) {
-      //   const idx = options.indexOf(option)
-      //   options.splice(idx, 1)
-      // }
-
-      // this.setState({ options: [...options] })
+      setOptions(newOptions)
     }
   }
 
-  shrinkText () {
-    let { text, options } = this.state
-
+  function shrinkText () {
     if (!text) {
       return ''
     }
 
+    let shrunkText = text
+
     options.forEach(optionId => {
       const textOption = textOptions.find(o => o.id === optionId)
       if (textOption) {
-        text = textOption.fn(text)
+        shrunkText = textOption.fn(shrunkText)
       }
     })
 
-    return text
+    return shrunkText
   }
 
-  render () {
-    const { text } = this.state
-    const shrunkText = this.shrinkText()
-
-    return (
-      <div className='mw8 pa3 center sans-serif'>
-        <h1 className='f-subheadline lh-solid ma0'>TweetShrink</h1>
-        <TextDisplay
-          text={text}
-          shrunkText={shrunkText}
-          onTextUpdate={this.handleTextUpdate}
-        />
-        <div className='flex flex-wrap'>
-          <div className='w-100'>
-            <h2 className='f2 lh-copy normal ma0 pb2'>Options</h2>
-          </div>
-          {textOptions.map((option, idx) => (
-            <TextOptionField key={idx} option={option} onChange={this.setOption(option.id)} />
-          ))}
+  return (
+    <div className='mw8 pa3 center sans-serif'>
+      <h1 className='f-subheadline lh-solid ma0'>TweetShrink</h1>
+      <TextDisplay
+        text={text}
+        shrunkText={shrinkText()}
+        onTextUpdate={e => setText(e.target.value)}
+      />
+      <div className='flex flex-wrap'>
+        <div className='w-100'>
+          <h2 className='f2 lh-copy normal ma0 pb2'>Options</h2>
         </div>
+        {textOptions.map((option, idx) => (
+          <TextOptionField key={idx} option={option} onChange={setOption(option.id)} />
+        ))}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
